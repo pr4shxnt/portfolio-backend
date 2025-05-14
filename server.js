b295725270585038c3bbcaca50ae57153b99ffb2
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-05-14 18:12:47 +0545

    cloudinary integrated + projects

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./Database/DBconnect');
+const serverless = require('serverless-http');
 
 const app = express();
 
@@ -9,22 +10,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+ connectDB();
 
-connectDB();
+ app.use('/api/projects', require('./Routes/projectRoutes'));
 
-app.use('/api/projects', require('./Routes/projectRoutes'));
-
-
-if (process.env.NODE_ENV != 'production') {
-  app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+ if (process.env.NODE_ENV === 'production') {
+   module.exports.handler = serverless(app);
+} else {
+   app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
-
-
-module.exports = app;
